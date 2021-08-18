4f5180b570c678fc5d73b8b0d86f3198120f2678
lxg <l1753531985@gmail.com>
2021-08-18 05:52:12 -0400

    加入迭代器类后bug未调

--- Vector.cpp
+++ Vector.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdexcept>
+#include <stack>
+#include <unistd.h>
 
 using namespace std;
 
@@ -7,8 +9,7 @@ template <typename Object>
 class Vector
 {
 public:
-	explicit Vector(int initSize = 0) 
-		: theSize(initSize), theCapacity(initSize + SPARE_CAPACITY)
+	explicit Vector(int initSize = 0) : theSize(initSize), theCapacity(initSize + SPARE_CAPACITY)
 	{ objects = new Object[theCapacity]; }
 	Vector(const Vector& rhs) : objects(NULL) { operator=(rhs); }
 	~Vector() { delete[] objects; }
@@ -84,8 +85,59 @@ public:
 	const Object& back() const
 	{ return objects[theSize-1]; }
 
-	typedef Object* iterator;
-	typedef const Object* const_iterator;
+	//typedef Object* iterator;
+	//typedef const Object* const_iterator;
+	class const_iterator
+	{
+		protected:
+			Object* current;
+			Object& retrieve() const
+			{ return *current; }
+			const_iterator(Object* p) : current{p} {}
+		public:
+			const_iterator() : current(NULL) {}
+			const Object& operator*() const 
+			{ return retrieve(); }	
+			const_iterator& operator++()
+			{
+				current++;
+				return *this;
+			}
+			const_iterator operator++(int)
+			{
+				const_iterator old = *this;
+				++(*this);
+				return old;
+			}
+			bool operator==(const const_iterator& rhs) const
+			{ return *current == *(rhs.current); }
+			bool operator!=(const const_iterator& rhs) const
+			{ return !(*this == rhs); }
+	};
+
+	class iterator : public const_iterator
+	{
+		public: 
+			iterator() {}
+			Object& operator*()
+			{ return const_iterator::retrieve(); }
+			const Object& operator*() const
+			{ return const_iterator::operator*(); }
+			iterator& operator++()
+			{
+				current++;
+				return *this;
+			}
+
+			iterator operator++(int)
+			{ 
+				iterator old = *this;
+				++(*this);
+				return old;
+			}
+		protected:
+			iterator(Object* p) : const_iterator(p) {}
+	};
 
 	iterator begin()
 	{ return &objects[0]; }
@@ -100,6 +152,48 @@ public:
 	{ return &objects[size()]; }
 
 	enum { SPARE_CAPACITY = 16 };
+
+	void insert(const Object& x, int index)
+	{
+		stack<int> tmp;
+		if (index < 0) index = 0; 
+		if (index > theSize) index = theSize;
+		int times = theSize - index;
+		while (times)
+		{ 
+			//sleep(1);
+			tmp.push(back()); 
+			pop_back(); 
+			times--;
+		}
+		push_back(x);
+		while (!tmp.empty()) 
+		{ 
+			push_back(tmp.top()); 
+			tmp.pop(); 
+		}
+		theSize++;
+	}
+
+	void erase(int index)
+	{
+		stack<int> tmp;
+		if (index < 0 || index > theSize) return;
+		int times = theSize - index;
+		while (times)
+		{ 
+			tmp.push(back()); 
+			pop_back(); 
+			times--;
+		}
+		tmp.pop();
+		while (!tmp.empty()) 
+		{ 
+			push_back(tmp.top()); 
+			tmp.pop(); 
+		}
+		theSize--;
+	}
 private:	
 	int theSize;
 	int theCapacity;
@@ -123,9 +217,19 @@ int main()
 	vi.push_back(8);
 	vi.push_back(-9);
 	vi.push_back(0);
+	vi.push_back(7);
 	//for (int i = 0; i < vi.size(); i++)
 		//cout << vi[i] << endl;
 	Vector<int>::iterator viter;
+	for (int i = 0; i < vi.size(); i++)
+		cout << vi[i] << " ";
+	cout << endl;
+	vi.insert(-3, -5);
+	vi.erase(6);
+	for (int i = 0; i < vi.size(); i++)
+		cout << vi[i] << " ";
+	cout << endl;
+	/*
 	cout << "what num you want find: ";
 	int num = 0;
 	cin >> num;
@@ -134,6 +238,7 @@ int main()
 		cout << "not find" << endl;
 	else 
 		cout << "find it" << endl;
+	*/
 	return 0;
 }
 
