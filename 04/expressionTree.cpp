c7a27446c047e1063119e4bf5b23f52a3ef9ee07
lxg <l1753531985@gmail.com>
2021-09-07 02:04:27 -0400

    表达树完成

--- 04/expressionTree.cpp
+++ 04/expressionTree.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
+#include <stack>
 #include <sstream>
 
 using namespace std;
 
 template <typename Object>
 struct Node {
-	Object element;
-	Node* left;
-	Node* right;
+	Object data;
+	Node<Object>* left;
+	Node<Object>* right;
 };
 
-class Tree {
-};
+void createExpressionTree(string expression, stack<Node<string>*>& dataStore)
+{
+	stringstream data{expression};	
+	char singleExpression;
+	while (data >> singleExpression)
+	{
+		string tmp = "";
+		tmp.push_back(singleExpression);
+		Node<string>* n = new Node<string>;
+		n->data = tmp;
+		if (ispunct(singleExpression))
+		{
+			n->right = dataStore.top();
+			dataStore.pop();
+			n->left = dataStore.top();
+			dataStore.pop();
+		}
+		dataStore.push(n);
+	}
+}
+
+template <typename Object>
+void traverseTree(Node<Object>* node)
+{
+	delete node;
+	if (!node->left && !node->right) return;
+	traverseTree(node->left);
+	traverseTree(node->right);
+}
 
 int main()
 {
 	string expression = "a b + c d e + * *";
-	stringstream data{expression};	
-	string singleExpression = "";
-	while (data >> singleExpression)
-	{
-		cout << singleExpression << '\t';
-	}
-	cout << endl;
+	stack<Node<string>*> dataStore;
+	createExpressionTree(expression, dataStore);
+	traverseTree(dataStore.top());
 	return 0;
 }
