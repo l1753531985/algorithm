8b6d8fdeaa7b107d89f3f1f3414615c92469f97e
l1753531985 <l1753531985@gmail.com>
2022-05-19 02:39:03 -0400

    has not finished

--- matrixProducts.cpp
+++ matrixProducts.cpp
@@ -3,6 +3,10 @@
 #include <unordered_map>
 #include <stdio.h>
 
+#define COLSUFF "colSpace"
+#define ROWSUFF "rowSpace"
+#define SUFFSIZE 7 
+
 using namespace std;
 
 class MatrixArithemtic {
@@ -14,14 +18,16 @@ private:
 	};
 	unordered_map<string,matrixInfo> matricesInfo;
 	vector<int> matricsData;
-	void getColVector(string name, int colIndex, vector<int>& colVector);
-	void setColVector(string src, string res, int colIndex, int coff);
-	void matrixProducts(string matrixA, string matrixB);	
+	void getColumnSpace(string matrixName);
+	void matrixProduct(string matrixNameA, string matrixNameB);
 public:
-	explicit MatrixArithemtic(){};
+	explicit MatrixArithemtic(const vector<int>& matrix, int row, int col, string name)
+	{ addMatrix(matrix, row, col, name); }
 	void addMatrix(const vector<int>& matrix, int row, int col, string name);
-	void getMatrixProducts(string matrixA, string matrixB);
-	void showMatrix(string matrixName);
+	void getColumnVector(string matrixName, vector<int>& colVector, int colIndex);
+	void getRowVector(string matrixName, vector<int>& rowVector, int rowIndex);
+	void showMatrix(string matrixName, int isTranspose);
+	void getMatrixProduct(string matrixNameA, string matrixNameB, vector<int>& res); 
 	~MatrixArithemtic(){};
 };
 
@@ -54,86 +60,106 @@ void MatrixArithemtic::addMatrix(const vector<int>& matrix, int row, int col, st
 		matricsData.push_back(elem);
 }
 
-void MatrixArithemtic::getColVector(string name, int colIndex, vector<int>& colVector)
+// transpose matrix get column space and find column vector easily
+void MatrixArithemtic::getColumnSpace(string matrixName)
 {
-	matrixInfo m = matricesInfo[name];
-	for (int rowIndex = 0; rowIndex < m.row; rowIndex++)
+	vector<int> colSpace;
+	// It must be exists in matrices data, if the transpose matrix can be found.
+	if (matricesInfo.find(matrixName) != matricesInfo.end())
 	{
-		int elem = m.startIndex + rowIndex*(m.col)  + colIndex;	 
-		colVector.push_back(matricsData[elem]);
-		//printf("%d\n",matricsData[elem]);
-	}
-}
-
-void MatrixArithemtic::setColVector(string src, string res, int colIndex, int coff)
-{
-	matrixInfo s = matricesInfo[src];
-	matrixInfo r = matricesInfo[res];
-	for (int rowIndex = 0; rowIndex < r.row; rowIndex++)
-	{
-		int elemPostion = rowIndex*(s.col)  + colIndex;	 
-		matricsData[r.startIndex+elemPostion] = matricsData[s.startIndex+elemPostion] * coff;
-	}
-}
-
-void MatrixArithemtic::matrixProducts(string matrixA, string matrixB)	
-{
-	// add result matrix into matrices data and info
-	string resName = matrixA+matrixB;
-	matrixInfo mib = matricesInfo[matrixB];
-	matrixInfo mia = matricesInfo[matrixA];
-	vector<int> res(mia.row*mib.col);
-	addMatrix(res, mia.row, mib.col, resName);
-
-	// copy matrix A into matrix result
-	for (int i = 0; i < mia.col*mia.row; i++)
-		matricesData[] = matricsData[i];
-
-	// split matrixA into column space multiple the column space of matrixB  
-	for (int i = 0; i < mib.col; i++)
-	{
-		vector<int> colVector;
-		getColVector(matrixB, i, colVector);
-		for (int j = 0; j < colVector.size(); j++)
-			if (colVector[j])
+		matrixInfo mi = matricesInfo[matrixName];
+		for (int i = 0; i < mi.col; i++)
+		{
+			for (int j = 0; j < mi.row; j++)
 			{
-				setColVector(matrixA, resName, i, colVector[j]);
+				int elemPos = mi.startIndex + j*mi.col + i;
+				colSpace.push_back(matricsData[elemPos]);
 			}
+		}
+		// add colSpace into matrices data for find it next
+		string name = (matrixName.find(COLSUFF) == -1) ? COLSUFF+matrixName : matrixName.substr(matrixName.find(COLSUFF)+SUFFSIZE+1,(matrixName.length()-matrixName.find(COLSUFF)-SUFFSIZE));
+		addMatrix(colSpace, mi.col, mi.row, name);
 	}
 }
 
-void MatrixArithemtic::showMatrix(string matrixName)
+void MatrixArithemtic::getColumnVector(string matrixName, vector<int>& colVector, int colIndex)
+{
+	getColumnSpace(matrixName);
+	string colSpaceName = COLSUFF + matrixName;
+	getRowVector(colSpaceName, colVector, colIndex);
+}
+
+void MatrixArithemtic::getRowVector(string matrixName, vector<int>& rowVector, int rowIndex)
 {
 	matrixInfo mi = matricesInfo[matrixName];
-	for (int i = mi.startIndex; i < mi.startIndex+(mi.col*mi.row); i++)
+	int index = mi.startIndex + (rowIndex-1)*mi.col;
+	for (int i = index; i < index+mi.col; i++)
+		rowVector.push_back(matricsData[i]);
+}
+
+void MatrixArithemtic::matrixProduct(string matrixNameA, string matrixNameB)
+{
+	// pre process for get column space  
+	vector<int> product;
+	matrixInfo mia = matricesInfo[COLSUFF+matrixNameA];
+	matrixInfo mib = matricesInfo[COLSUFF+matrixNameB];
+	for (int i = 0; i < mib.col; i++)
 	{
-		if (!(i % mi.col)) printf("\n");
+		vector<int> colVectorB;
+		getColumnVector(matrixNameB, colVectorB, i);
+		for (int elem : colVectorB)
+			if (elem)
+			{
+			}
+	}
+
+	// add transpose matrix of matrix products into matrix data for find it next
+	string colName = COLSUFF+matrixNameA+matrixNameB;
+	addMatrix(product, mia.row, mib.col, colName); 
+	// add matrix products into matrix data for find it next
+	getColumnSpace(colName);
+}
+
+void MatrixArithemtic::getMatrixProduct(string matrixNameA, string matrixNameB, vector<int>& res) 
+{
+	matrixProduct(matrixNameA, matrixNameB);
+	matrixInfo imab = matricesInfo[matrixNameA+matrixNameB];
+	for (int i = imab.startIndex; i < imab.startIndex+imab.row*imab.col; i++)
+		res.push_back(matricsData[i]);
+}
+
+// isTranspose for find the column space 
+void MatrixArithemtic::showMatrix(string matrixName, int isTranspose = 0)
+{
+	if (isTranspose) matrixName = COLSUFF + matrixName;
+	// if it has a record about the matrix, matrices data will return the record.
+	// if not found the key, it will occur a error
+	if (matricesInfo.find(matrixName) == matricesInfo.end()) 
+	{
+		printf("It is no such matrix!");
+		return;
+	}
+	matrixInfo mi = matricesInfo[matrixName];
+	for (int i = mi.startIndex; i < mi.startIndex+mi.row*mi.col; i++)
+	{
+		if (!(i % mi.row)) printf("\n");
 		printf("%d\t",matricsData[i]);
 	}
 	printf("\n");
 }
 
-void MatrixArithemtic::getMatrixProducts(string matrixA, string matrixB)
-{
-	matrixInfo mia = matricesInfo[matrixA];
-	matrixInfo mib = matricesInfo[matrixB];
-	if (mia.row != mib.col || mia.col != mib.row) 
-	{
-		printf("the matrix A and B can not get products\n");
-		return;
-	}
-	matrixProducts(matrixA, matrixB);
-	showMatrix(matrixA+matrixB);
-}
 
 int main()
 {
 	// 5x3 matrix
-	vector<int> A{1,-1, 0,1};
-	vector<int> B{3,2,0,-2};
-	MatrixArithemtic ma;
-	ma.addMatrix(A, 2, 2, "A");
-	ma.addMatrix(B, 2, 2, "B");
-	ma.getMatrixProducts("A","B");
+	vector<int> A{4,0,3,-1,1,-2};
+	vector<int> B{3,2,-1,9,0,-2};
+	MatrixArithemtic ma{A,2,3,"A"};
+	ma.addMatrix(B,3,2,"B");
+	vector<int> test;
+	ma.getMatrixProduct("A","B",test);
+	for (int elem:test)
+		printf("%d\t",elem);
+	printf("\n");
 	return 0;
 }
