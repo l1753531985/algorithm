8bd9532159a9a6eea2d8b5ee53a5006a628d862b
root <root@debian.debian>
2022-08-31 09:14:56 -0400

    algorithm with template

--- chapter01/FixedCapacityStackOfStrings.h
+++ chapter01/FixedCapacityStackOfStrings.h
@@ -3,16 +3,55 @@
 #ifndef __FIXEDCAPACITYSTACKOFSTRING_H
 using namespace std;
 
+template <typename T>
 class FixedCapacityStackOfStrings {
 public:
-    FixedCapacityStackOfStrings(int cap);
-    ~FixedCapacityStackOfStrings();
-    void push(string item);
-    string pop();
-    bool isEmpty();
-    int size();
+    FixedCapacityStackOfStrings(int capacity):index{0},cap{capacity}
+	{ array = new T[cap]; }
+
+    ~FixedCapacityStackOfStrings()
+	{ delete[] array; }
+
+    void push(T item)
+	{
+		if (cap == index)
+		{
+			cap *= 2;
+			resize(cap);
+		}
+		array[index] = item; 
+		index++; 
+	}
+
+    T pop()
+	{ 
+		index--; 
+		T item = array[index];
+		if (index > 0 && index == (cap/4))
+		{
+			cap /= 2;
+			resize(cap);
+		}
+		return item;
+	}
+
+    bool isEmpty() 
+	{ return (index == 0); }
+
+    int size()
+	{ return index; }
+
+	void resize(int max)
+	{ 
+		T* temp= new T[max]; 
+		for (int i = 0; i < index; i++)
+			temp[i] = array[i];
+		delete[] array;
+		array = temp;
+	}
 private:
     int index;
-    string* array;
+	int cap;
+    T* array;
 };
 #endif
--- chapter01/testExample.cpp
+++ chapter01/testExample.cpp
@@ -1,7 +1,19 @@
 #include "FixedCapacityStackOfStrings.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int ac, char* argv[])
 {
-    FixedCapacityStackOfStrings s{100};
+	FixedCapacityStackOfStrings<string> s{100};
+	string token;
+	while (cin >>token)
+	{
+		if (token.compare("-"))
+			s.push(token);
+		else if (!s.isEmpty())
+			cout << s.pop() << " " << endl;
+	}
+	cout << "(" << s.size() << " left on stack)" << endl;
     return 0;
 }
